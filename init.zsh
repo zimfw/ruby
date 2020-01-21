5de50864ec9bfc8116a0768c0e94cd0b12c3ae0d
Eric Nielsen <eric@amalgamar.com.br>
2020-01-21 07:42:54 -0500

    Test before using
    Since modules are to be used by a community of people having a variety
    of environments, this should not expect users to have a specific setup
    and give errors if they don't, but try to infer the setup instead.

--- init.zsh
+++ init.zsh
@@ -2,29 +2,33 @@
 # Ruby aliases/settings
 #
 
-# Use updates openssl
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+() {
+  if (( ${+commands[brew]} )); then
+    # Use updates openssl
+    local -r openssl11_dir=$(brew --prefix openssl@1.1)
+    [[ -d ${openssl11_dir} ]] && export RUBY_CONFIGURE_OPTS="--with-openssl-dir=${openssl11_dir}"
+  fi
+  # Load rbenv into the shell session
+  (( ${+commands[rbenv]} )) && eval "$(rbenv init -)"
 
-# Load rbenv into the shell session
-eval "$(rbenv init -)"
+  #
+  # Aliases
+  #
 
-#
-# Aliases
-#
+  # General
+  alias rb='ruby'
 
-# General
-alias rb='ruby'
+  # rbenv
+  alias rbe='rbenv'
+  alias rbei='rbenv install'
 
-# rbenv
-alias rbe='rbenv'
-alias rbei='rbenv install'
-
-# Bundler
-alias rbb='bundle'
-alias rbbc='bundle clean'
-alias rbbe='bundle exec'
-alias rbbi='bundle install --path vendor/bundle'
-alias rbbl='bundle list'
-alias rbbo='bundle open'
-alias rbbp='bundle package'
-alias rbbu='bundle update'
+  # bundler
+  alias rbb='bundle'
+  alias rbbc='bundle clean'
+  alias rbbe='bundle exec'
+  alias rbbi='bundle install --path vendor/bundle'
+  alias rbbl='bundle list'
+  alias rbbo='bundle open'
+  alias rbbp='bundle package'
+  alias rbbu='bundle update'
+}
